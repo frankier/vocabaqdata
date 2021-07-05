bfb3809376a00445e50b15a22fdf056067b63f9e
Frankie Robertson <frankie@robertson.name>
2021-07-05 23:39:56 +0300

    Clickify enrich_svl12k

--- vocabaqdata/proc/enrich_svl12k.py
+++ vocabaqdata/proc/enrich_svl12k.py
@@ -1,13 +1,22 @@
-import sys
+import click
 
 import pandas
 
 from ..utils.freq import add_zipfs
 
-df = pandas.read_parquet(sys.argv[1])
-add_zipfs(df)
-df = df[df["respondent"] != 0]
-df["known"] = df["score"] >= 5
-df["unknown"] = df["score"] < 5
-df.reset_index(drop=True, inplace=True)
-df.to_feather(sys.argv[2])
+
+@click.command()
+@click.argument("df_in", type=click.Path(exists=True))
+@click.argument("df_out", type=click.Path())
+def main(df_in, df_out):
+    df = pandas.read_parquet(df_in)
+    add_zipfs(df)
+    df = df[df["respondent"] != 0]
+    df["known"] = df["score"] >= 5
+    df["unknown"] = df["score"] < 5
+    df.reset_index(drop=True, inplace=True)
+    df.to_parquet(df_out)
+
+
+if __name__ == "__main__":
+    main()
