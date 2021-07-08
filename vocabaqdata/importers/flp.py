20f36c207a31bdd9d97c8cf94438f4de00e56710
Frankie Robertson <frankie@robertson.name>
2021-07-08 14:23:19 +0300

    Fix flp importing

--- vocabaqdata/importers/flp.py
+++ vocabaqdata/importers/flp.py
@@ -2,6 +2,7 @@ from vocabaqdata.import_tools import mk_import_command
 
 
 main = mk_import_command(
+    filetype="tsv",
     encoding="iso-8859-1",
     names=["exp_id", "item_id", "spelling", "rt", "accuracy", "lexicality"]
 )
